2ec476c54c18a8095aa7fbc28efd4b44a6a0e23e
sneelyg <santiagoneely@gmail.com>
2022-09-13 00:26:23 +0000

    Almost to Todos

--- src/app.py
+++ src/app.py
@@ -1,2 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
+
+todos= [{ "label": "My first task", "done": False } ]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+   
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
